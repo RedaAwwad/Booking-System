43e7190a85e818ee660de99d380aee942b0bc02b
Reda Awwad <reda.awwad.eng@gmail.com>
2026-05-12 15:11:55 +0300

    chore: update README file

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import { SanitizePipe } from './common/pipes/sanitize.pipe';
+import { SanitizePipe } from './common/sanitize/sanitize.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
